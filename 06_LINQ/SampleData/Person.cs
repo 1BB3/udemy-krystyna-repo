8a3916cf0f3eac985d04eee3d74f486eb3cdbfc9
1BB3 <sanjeev1bb3@protonmail.com>
2025-02-05 16:58:27 +0545

    initial commit

---
+++ 06_LINQ/SampleData/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_LINQ.SampleData
+{
+    enum Occupation
+    {
+        Engineer,
+        Doctor,
+        Teacher,
+        Artist,
+        Lawyer,
+        Chef,
+        Scientist,
+        Athlete
+    }
+
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public double Weight { get; set; } // in kilograms
+        public double Height { get; set; } // in centimeters
+        public Occupation Occupation { get; set; }
+    }
+}
